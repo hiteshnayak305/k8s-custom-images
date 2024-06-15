c3ecf9ef895b01b617ce8cd18805e662ad241b12
Hitesh Nayak <hiteshnayak305@gmail.com>
2024-06-15 15:20:45 +0530

    feat: add cleanup of branches

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+## declare an array variable
+declare -a arr=("branch1" "branch2")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   echo "Deleting branch $i"
+   git push -d origin $i  && git branch -d $i
+done
+
+# You can access them using echo "${arr[0]}", "${arr[1]}" also
