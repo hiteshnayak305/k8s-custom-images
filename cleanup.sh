6e420fdd74c851f5612eafc4e0c387eab07d836d
Hitesh Nayak <hiteshnayak305@gmail.com>
2025-02-06 01:07:01 +0530

    chore(immich): upgrade image

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,7 @@ declare -a arr=( \
    "hiteshnayak305/inbound-agent@3273.v4cfe589b_fd83-1-alpine" \
    "hiteshnayak305/grafana@11.4.0" \
    "hiteshnayak305/immich-server@v1.123.0" \
+   "hiteshnayak305/immich-server@v1.124.2" \
 )
 
 ## now loop through the above array
--- mergepush.sh
+++ mergepush.sh
@@ -6,7 +6,7 @@ declare -a arr=( \
    "hiteshnayak305/eclipse-temurin@23-jdk-alpine" \
    "hiteshnayak305/gitea@1.22.3-rootless" \
    "hiteshnayak305/grafana@11.5.1" \
-   "hiteshnayak305/immich-server@v1.124.2" \
+   "hiteshnayak305/immich-server@v1.125.7" \
    "hiteshnayak305/inbound-agent@3283.v92c105e0f819-7-alpine" \
    "hiteshnayak305/jenkins@2.492.1-alpine" \
    "hiteshnayak305/k8s@1.31.4" \
