b78c379d4b8e8702f3da3fac6123dd4722100507
Hitesh Nayak <hiteshnayak305@gmail.com>
2025-03-15 00:08:52 +0530

    fix(docker): added entrypoint to wait for buildkit instance

---
+++ common/docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Wait for the BuildKit daemon to be ready
+while ! nc -z "$BUILDKIT_HOST" "$BUILDKIT_PORT"; do
+    echo "Waiting for $BUILDKIT_HOST:$BUILDKIT_PORT..."
+    sleep 5s
+done
+# Create a new BuildKit instance
+docker buildx create --name remote --driver remote --node remote-0 --use "tcp://$BUILDKIT_HOST:$BUILDKIT_PORT"
+# Inspect the BuildKit instance
+docker buildx inspect remote
+
+# Run the command
+exec "$@"
